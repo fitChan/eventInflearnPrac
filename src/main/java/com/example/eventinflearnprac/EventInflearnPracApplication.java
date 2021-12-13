59d4800aacb37a4c75ced7297c2426a3b415942a
fitChan <84306157+fitChan@users.noreply.github.com>
2021-12-13 20:08:24 +0900

    eventDomain, Test

--- src/main/java/com/example/eventinflearnprac/EventInflearnPracApplication.java
+++ src/main/java/com/example/eventinflearnprac/EventInflearnPracApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventInflearnPracApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EventInflearnPracApplication.class, args);
     }
---
+++ src/test/java/com/example/eventinflearnprac/EventTest.java
@@ -0,0 +1,37 @@
+package com.example.eventinflearnprac;
+
+import com.example.eventinflearnprac.events.Event;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Inflearn Spring Rest Api")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+
+        //Given
+        String name = "Event";
+        String description = "Spring";
+
+        //when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+
+    }
+
+}
