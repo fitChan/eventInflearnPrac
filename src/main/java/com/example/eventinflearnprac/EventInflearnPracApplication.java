113a134f145e278f90eb5a717a6a7c1707282721
fitChan <84306157+fitChan@users.noreply.github.com>
2021-12-13 19:18:50 +0900

    Initial commit

---
+++ src/main/java/com/example/eventinflearnprac/EventInflearnPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventinflearnprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventInflearnPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventInflearnPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventinflearnprac/EventInflearnPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventinflearnprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventInflearnPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
