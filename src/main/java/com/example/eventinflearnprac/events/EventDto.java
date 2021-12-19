a61aaf3938264ad60b7ddd26fe9f2a0f006a02b1
fitChan <84306157+fitChan@users.noreply.github.com>
2021-12-19 22:34:49 +0900

    valid check is not function

--- src/main/java/com/example/eventinflearnprac/events/EventDto.java
+++ src/main/java/com/example/eventinflearnprac/events/EventDto.java
@@ -1,25 +1,37 @@
 package com.example.eventinflearnprac.events;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor @AllArgsConstructor
 @Data @Builder
 public class EventDto {
 
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NotNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
     private LocalDateTime beginEventDateTime;
+    @NotNull
     private LocalDateTime endEventDateTime;
     private String location;
+    @Min(0)
     private int basePrice;
+    @Min(0)
     private int maxPrice;
+    @Min(0)
     private int limitOfEnrollment;
 
 }
