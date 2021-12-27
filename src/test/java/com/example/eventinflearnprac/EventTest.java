376dd10ff2a3caa7acd6441d06b8bcd9d938df03
fitChan <84306157+fitChan@users.noreply.github.com>
2021-12-27 20:50:39 +0900

    junit params are not function

--- src/test/java/com/example/eventinflearnprac/EventTest.java
+++ src/test/java/com/example/eventinflearnprac/EventTest.java
@@ -1,14 +1,21 @@
 package com.example.eventinflearnprac;
 
 import com.example.eventinflearnprac.events.Event;
+import org.junit.internal.runners.JUnit38ClassRunner;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EventTest {
 
     @Test
-    public void builder(){
+    public void builder() {
         Event event = Event.builder()
                 .name("Inflearn Spring Rest Api")
                 .description("REST API development with Spring")
@@ -17,7 +24,7 @@ public class EventTest {
     }
 
     @Test
-    public void javaBean(){
+    public void javaBean() {
 
         //Given
         String name = "Event";
@@ -34,55 +41,53 @@ public class EventTest {
 
     }
 
-    @Test
-    public void testFree(){
-        //Given
-        Event event = Event.builder().
-                basePrice(0)
-                .maxPrice(0)
-                .build();
-        //when
-        event.update();
 
-        //then
-        assertThat(event.isFree()).isTrue();
-
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
+    @ParameterizedTest
+    @MethodSource("paramsForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        // given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
+        // when
         event.update();
 
-        assertThat(event.isFree()).isFalse();
-
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(10)
-                .build();
-
-        event.update();
-
-        assertThat(event.isFree()).isFalse();
+        // then
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
 
-    @Test
-    public void testOffline(){
-        //Given
-        Event event = Event.builder().
-                location("장소있음")
+    @ParameterizedTest
+    @MethodSource("paramsForTestOffline")
+    void testOffline(String location, boolean isOffline) {
+        // given
+        Event event = Event.builder()
+                .location(location)
                 .build();
-        //when
+
+        // when
         event.update();
 
-        assertThat(event.isOffline()).isTrue();
+        // then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
 
-        event = Event.builder().
-                build();
-        //when
-        event.update();
+    private static Stream<Arguments> paramsForTestFree() { // argument source method
+        return Stream.of(
+                Arguments.of(0,0, true),
+                Arguments.of(100, 0, false),
+                Arguments.of(0, 100, false),
+                Arguments.of(100, 200, false)
+        );
+    }
 
-        assertThat(event.isOffline()).isFalse();
+    private static Stream<Arguments> paramsForTestOffline() { // argument source method
+        return Stream.of(
+                Arguments.of("강남", true),
+                Arguments.of(null, false),
+                Arguments.of("        ", false)
+        );
     }
 
 }
